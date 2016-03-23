adc643f1258560fde1d1060af86f2ca9e2274b69
Joe Powell <joe2580.com@googlemail.com>
2016-03-23 21:16:40 +0000

    Added configuration file support

--- server.js
+++ server.js
@@ -2,13 +2,17 @@
 var express = require("express");
 var morgan  = require("morgan");
 
+// load our configuration
+var config = require("./config/config.js")
+var port = config.http_port;
+
 // setup express app
 var app = express();
 var port = process.env.PORT || 8080;
 app.use(morgan("dev"));
 
 // setup routes
-require("./app/routes.js")(app);
+require("./app/routes.js")(app, config);
 
 // start server
 app.listen(port);
--- services/databaseService.js
+++ services/databaseService.js
@@ -1,12 +1,9 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
-});
+// load configuration
+var config = require("./config/config.js")
+
+var connection = mysql.createConnection(config.database);
 
 var getStatusInformation = function(){
     return 'getStatusInformation';
