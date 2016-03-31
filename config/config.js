8753eb0347441229cd45d9cad5cd9f80f17fec1a
Geanina Hinta <geaninahinta@yahoo.com>
2016-04-01 00:09:38 +0300

    Azure can now deploy the project

---
+++ config/config.js
@@ -0,0 +1,22 @@
+// example configuration file
+module.exports = {
+  // http website port
+  http_port: 80,
+
+  // https website port
+  tls_port: 443,
+
+  // database configuration 
+  database: {
+    host: process.env.DATABASE_HOSTNAME,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+  },
+
+  sendGrid: {
+    username: process.env.SENDGRID_USERNAME,
+    password: process.env.SENDGRID_PASSWORD
+  }
+};
