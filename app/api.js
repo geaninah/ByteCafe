649beb245a7901ef973b39de29d825ee716ec95d
Joe Powell <joe2580.com@googlemail.com>
2016-04-01 19:17:37 +0100

    Fixed indentation in api.js

--- app/api.js
+++ app/api.js
@@ -11,99 +11,99 @@ module.exports = function (database, email) {
   return {
     // present some debug info
     hello: function(req, res) {
-        res.header("Content-Type", "text/plain; charset=utf-8");
-        res.write("Hello World!\n");
-        res.write("User: "    + process.env.USER + "\n");
-        res.write("Dirname: " + __dirname        + "\n");
-        res.end("ProcID: "    + process.pid      + "\n");
+      res.header("Content-Type", "text/plain; charset=utf-8");
+      res.write("Hello World!\n");
+      res.write("User: "    + process.env.USER + "\n");
+      res.write("Dirname: " + __dirname        + "\n");
+      res.end("ProcID: "    + process.pid      + "\n");
     },
 
     // fetch the terms and conditions
     terms: function(req, res) {
-        res.header("Content-Type", "text/html; charset=utf-8");
-        res.sendFile(path.join(__dirname, "../config/terms.txt"));
+      res.header("Content-Type", "text/html; charset=utf-8");
+      res.sendFile(path.join(__dirname, "../config/terms.txt"));
     },
 
     // placeholder for not implemented functionality
     notImplemented: function(req, res) {
-        res.header("Content-Type", "text/plain; charset=utf-8");
-        res.end("Not implemented!\n");
+      res.header("Content-Type", "text/plain; charset=utf-8");
+      res.end("Not implemented!\n");
     },
 
     // return status information
     status: function(req, res) {
-        res.header("Content-Type", "text/plain; charset=utf-8");
-        res.end("all good g\n");
+      res.header("Content-Type", "text/plain; charset=utf-8");
+      res.end("all good g\n");
     },
 
     // returns a list of cafes
     getCafes: function(req, res) {
-        database.getCafes(function(err, cafes){
-            if(!err){
-                res.header("Content-Type", "application/json; charset=utf-8");
-                res.end(JSON.stringify(cafes));
-            }
-        });
+      database.getCafes(function(err, cafes){
+        if(!err){
+          res.header("Content-Type", "application/json; charset=utf-8");
+          res.end(JSON.stringify(cafes));
+        }
+      });
     },
 
     // returns information about a cafe
     getCafeInfo: function(req, res) {
-        database.getCafeInfo(function(err, cafes){
-            if(!err){
-                res.header("Content-Type", "application/json; charset=utf-8");
-                res.end(JSON.stringify(cafes));
-            }
-        }, req.params.cafeId);
+      database.getCafeInfo(function(err, cafes){
+        if(!err){
+          res.header("Content-Type", "application/json; charset=utf-8");
+          res.end(JSON.stringify(cafes));
+        }
+      }, req.params.cafeId);
     },
 
     // returns a list of products at a cafe
     getProducts: function(req, res) {
-        database.getProducts(function(err, products){
-            if(!err){
-                res.header("Content-Type", "application/json; charset=utf-8");
-                res.end(JSON.stringify(products));
-            }
-        }, req.params.cafeId);
+      database.getProducts(function(err, products){
+        if(!err){
+          res.header("Content-Type", "application/json; charset=utf-8");
+          res.end(JSON.stringify(products));
+        }
+      }, req.params.cafeId);
     },
 
     // returns information about a products
     getProductInfo: function(req, res) {
-        database.getProductInfo(function(err, products){
-            if(!err){
-                res.header("Content-Type", "application/json; charset=utf-8");
-                res.end(JSON.stringify(products));
-            }
-        }, req.params.productId);
+      database.getProductInfo(function(err, products){
+        if(!err){
+          res.header("Content-Type", "application/json; charset=utf-8");
+          res.end(JSON.stringify(products));
+        }
+      }, req.params.productId);
     },
 
     // returns queued orders at a cafe
     getOrders: function(req, res) {
-        database.getOrders(function(err, orders){
-            if(!err){
-                res.header("Content-Type", "application/json; charset=utf-8");
-                res.end(JSON.stringify(orders));
-            }
-        }, req.params.cafeId);
+      database.getOrders(function(err, orders){
+        if(!err){
+          res.header("Content-Type", "application/json; charset=utf-8");
+          res.end(JSON.stringify(orders));
+        }
+      }, req.params.cafeId);
     },
 
     // returns information about a specific order
     getOrderInfo: function(req, res) {
-        database.getOrderInfo(function(err, orders){
-            if(!err){
-                res.header("Content-Type", "application/json; charset=utf-8");
-                res.end(JSON.stringify(orders));
-            }
-        }, req.params.orderId);
+      database.getOrderInfo(function(err, orders){
+        if(!err){
+          res.header("Content-Type", "application/json; charset=utf-8");
+          res.end(JSON.stringify(orders));
+        }
+      }, req.params.orderId);
     },
 
     // returns basket information for a specific user
     getBasket: function(req, res) {
-        database.getBasket(function(err, basket){
-            if(!err){
-                res.header("Content-Type", "application/json; charset=utf-8");
-                res.end(JSON.stringify(basket));
-            }
-        }, req.user.user_id);
+      database.getBasket(function(err, basket){
+        if(!err){
+          res.header("Content-Type", "application/json; charset=utf-8");
+          res.end(JSON.stringify(basket));
+        }
+      }, req.user.user_id);
     }
   }
 };
